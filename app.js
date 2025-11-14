fc406839bc973f26aca551d610167658d13377a7
Robbie McClellan <rlm8291@gmail.com>
2025-11-14 17:14:00 -0500

    Setup cards with ITEEA dataset

---
+++ app.js
@@ -0,0 +1,16 @@
+console.log("TESTING")
+
+import data from "./data.json" with { type: "json" }
+
+document.addEventListener("alpine:init", () => {
+  Alpine.data("iteea", () => ({
+    selectedView: -1,
+    clickMe(val) {
+      this.selectedView = val
+      console.log(this.selectedView)
+    },
+    searchResults() {
+      return data
+    }
+  }))
+})
