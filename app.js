6542ab6f83ee756a9f1edae15891602197ae60e2
Robbie McClellan <rlm8291@gmail.com>
2025-11-14 19:04:43 -0500

    Setup search functionality within cards

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ console.log("TESTING")
 
 import data from "./data.json" with { type: "json" }
 
+const list = new Fuse(data, { keys: ["Benchmark", "Benchmark Detail", "Benchmark Id"] })
+
 document.addEventListener("alpine:init", () => {
   Alpine.data("iteea", () => ({
     selectedItem: data[0],
@@ -9,8 +11,17 @@ document.addEventListener("alpine:init", () => {
       this.selectedItem = val
       console.log(this.selectedItem)
     },
+    search: "",
+    standard_selected: "All Standards",
+    grade_selected: "All Grades",
     searchResults() {
-      return data
+      let items = this.search ? list.search(this.search).map(val => val.item) : data
+
+      return items
+        .filter(val => (
+          (this.standard_selected === "All Standards" || val["Benchmark Id"].includes(this.standard_selected)) &&
+          (this.grade_selected === "All Grades" || val["Grade Level"] === this.grade_selected)
+        ))
     }
   }))
 })
